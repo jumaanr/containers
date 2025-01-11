2946f7348182ea3d821cb943d0dd8df7b90b781d
jumaanr <jumaanr@outlook.com>
2025-01-11 11:10:01 +0400

    cka exam attemp 110125

--- Kubernetes/linux_hacks.sh
+++ Kubernetes/linux_hacks.sh
@@ -4,3 +4,11 @@ $ docker ps | grep nginx
 
 #list the processes running
 ps aux
+
+#GitHacks
+git branch #list down tit local branches
+git branch -r #list down git remote branches
+git branch -a #list down all git local and remote branches
+git branch -d branch_name #delete a git branch
+git branch -D branch_name #force delete a git branch
+
